53f5e290930ef186a3ecdfc6abf44df1aeb858a1
Bastián <bastian.lluch@gmail.com>
2024-12-10 17:17:23 -0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
-        path: '/dashboard',
+        path: 'dashboard',
         loadComponent: () => import('./shared/components/layout/layout.component'),
         children: [
             {
